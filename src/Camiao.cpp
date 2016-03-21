023185d0d78e0a11a77b37996ce3af22b53081f1
pingudiogo <digax96@gmail.com>
2016-03-21 18:13:09 +0000

    Created Camiao class

---
+++ src/Camiao.cpp
@@ -0,0 +1,21 @@
+/*
+ * Camiao.cpp
+ *
+ *  Created on: 21/03/2016
+ *      Author: Diogo
+ */
+
+#include "Camiao.h"
+
+namespace std {
+
+Camiao::Camiao() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Camiao::~Camiao() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ src/Camiao.h
@@ -0,0 +1,22 @@
+/*
+ * Camiao.h
+ *
+ *  Created on: 21/03/2016
+ *      Author: Diogo
+ */
+
+#ifndef SRC_CAMIAO_H_
+#define SRC_CAMIAO_H_
+
+namespace std {
+
+class Camiao {
+	double cUtil,cOcupada;
+public:
+	Camiao();
+	virtual ~Camiao();
+};
+
+} /* namespace std */
+
+#endif /* SRC_CAMIAO_H_ */
