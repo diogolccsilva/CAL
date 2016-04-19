447bcff71f7152bfdcc33985c63ac47b8ebc8732
centeio <c.centeiojorge@outlook.pt>
2016-04-19 11:42:13 +0100

    Class Info

---
+++ src/Info.cpp
@@ -0,0 +1,18 @@
+/*
+ * Info.cpp
+ *
+ *  Created on: 19/04/2016
+ *      Author: Carolina
+ */
+
+#include "Info.h"
+
+Info::Info() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Info::~Info() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Info.h
@@ -0,0 +1,17 @@
+/*
+ * Info.h
+ *
+ *  Created on: 19/04/2016
+ *      Author: Carolina
+ */
+
+#ifndef SRC_INFO_H_
+#define SRC_INFO_H_
+
+class Info {
+public:
+	Info();
+	virtual ~Info();
+};
+
+#endif /* SRC_INFO_H_ */
