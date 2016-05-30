24c5c0d9abcee12419e4108d20d40baa1baca627
Tiago Bernardes Almeida <tiagoalmeida.95@hotmail.com>
2016-05-30 03:05:13 +0100

    Vertex doxygen.

--- src/Vertex.h
+++ src/Vertex.h
@@ -20,31 +20,68 @@ class Vertex {
 	bool removeEdgeTo(Vertex<T> *d);
 	//vector<Contentor*> check();
 public:
+
+	/**
+	 * Vertex constructor.
+	 * @param in Class to represent the vertex info.
+	 */
 	Vertex(T in);
+
+	/**
+	 * Default constructor.
+	 */
 	Vertex();
+
+	/**
+	 * Displays on the screen all the information of the vertex.
+	 */
 	void display() const;
 //	bool applyEco();
 
+	/**
+	 *
+	 * @return The vector with the adjacent edges.
+	 */
 	const vector<Edge<T> >& getAdj() const {
 		return adj;
 	}
 
+	/**
+	 *
+	 * @param adj Vector of edges.
+	 */
 	void setAdj(const vector<Edge<T> >& adj) {
 		this->adj = adj;
 	}
 
+	/**
+	 *
+	 * @return The object of the class representing the vertex info.
+	 */
 	T getInfo() const {
 		return info;
 	}
 
+	/**
+	 *
+	 * @param info Object of the class that represents the vertex info.
+	 */
 	void setInfo(T info) {
 		this->info = info;
 	}
 
+	/**
+	 *
+	 * @return True if the vertex was visited before or false otherwise.
+	 */
 	bool isVisited() const {
 		return visited;
 	}
 
+	/**
+	 *
+	 * @param visited True if the vertex was visited or false otherwise.
+	 */
 	void setVisited(bool visited) {
 		this->visited = visited;
 	}
